d0fab300f648705df88c1d2b752986abc8ed60bb
Steve Safarik <ssafarik@uw.edu>
2013-03-26 17:25:03 -0700

    Add some test code to print all camera feature values.

--- src/camnode.cpp
+++ src/camnode.cpp
@@ -37,6 +37,13 @@ typedef camera_aravis::CameraAravisConfig Config;
 
 static gboolean emit_software_trigger_callback (void *);
 
+typedef struct
+{
+	const char *szName;
+	const char *szTag;
+	ArvDomNode *pNode;
+	ArvDomNode *pNodeSibling;
+} NODEEX;
 
 // Global variables -------------------
 struct global_s
@@ -520,6 +527,121 @@ static gboolean periodic_task_cb (void *applicationdata)
     return TRUE;
 }
 
+// Get the child, where <p___> indicates an indirection, and the child's sibling.
+NODEEX GetGcFirstChild(ArvGc *pGenicam, NODEEX nodeex)
+{
+	const char *szName=0;
+
+	if (nodeex.pNode)
+	{
+		nodeex.pNode = arv_dom_node_get_first_child(nodeex.pNode);
+		if (nodeex.pNode)
+		{
+			nodeex.szName = arv_dom_node_get_node_name(nodeex.pNode);
+			nodeex.pNodeSibling = arv_dom_node_get_next_sibling(nodeex.pNode);
+
+			// Do the indirection.
+			if (nodeex.szName[0]=='p' && strcmp("pInvalidator", nodeex.szName))
+			{
+				szName = arv_dom_node_get_node_value(arv_dom_node_get_first_child(nodeex.pNode));
+				nodeex.pNode  = (ArvDomNode *)arv_gc_get_node(pGenicam, szName);
+				nodeex.szTag = arv_dom_node_get_node_name(nodeex.pNode);
+			}
+			else
+			{
+				nodeex.szTag = nodeex.szName;
+			}
+		}
+		else
+			nodeex.pNodeSibling = NULL;
+	}
+	else
+	{
+		nodeex.szName = NULL;
+		nodeex.szTag = NULL;
+		nodeex.pNodeSibling = NULL;
+	}
+	
+	//ROS_INFO("GFC name=%s, node=%p, sib=%p", szNameChild, nodeex.pNode, nodeex.pNodeSibling);
+
+	
+	return nodeex;
+}
+
+// Get the sibling, where <p___> indicates an indirection, and the sibling's sibling.
+NODEEX GetGcNextSibling(ArvGc *pGenicam, NODEEX nodeex)
+{
+	const char *szName=0;
+
+	// Go to the sibling.
+	nodeex.pNode = nodeex.pNodeSibling;
+	if (nodeex.pNode)
+	{
+		nodeex.szName = arv_dom_node_get_node_name(nodeex.pNode);
+		nodeex.pNodeSibling = arv_dom_node_get_next_sibling(nodeex.pNode);
+
+		// Do the indirection.
+		if (nodeex.szName[0]=='p' && strcmp("pInvalidator", nodeex.szName))
+		{
+			szName = arv_dom_node_get_node_value(arv_dom_node_get_first_child(nodeex.pNode));
+			nodeex.pNode = (ArvDomNode *)arv_gc_get_node(pGenicam, szName);
+			nodeex.szTag = nodeex.pNode ? arv_dom_node_get_node_name(nodeex.pNode) : NULL;
+		}
+		else
+		{
+			nodeex.szTag = nodeex.szName;
+		}
+	}
+	else 
+	{
+		nodeex.szName = NULL;
+		nodeex.szTag = NULL;
+		nodeex.pNodeSibling = NULL;
+	}
+
+	//ROS_INFO("GNS name=%s, node=%p, sib=%p", nodeex.szName, nodeex.pNode, nodeex.pNodeSibling);
+
+	
+	return nodeex;
+}
+
+// Walk the DOM tree.
+void PrintDOMTree(ArvGc *pGenicam, NODEEX nodeex, int nIndent)
+{
+	char		*szIndent=0;
+	const char *szFeature=0;
+	const char *szDomName=0;
+	const char *szFeatureValue=0;
+	
+	szIndent = new char[nIndent+1];
+	memset(szIndent,' ',nIndent);
+	szIndent[nIndent]=0;
+
+	nodeex = GetGcFirstChild(pGenicam, nodeex);
+	if (nodeex.pNode)
+	{
+		do
+		{
+			// Get current values.
+			//p = arv_dom_node_get_first_child(nodeex.pNode);
+
+
+			if (ARV_IS_GC_FEATURE_NODE((ArvGcFeatureNode *)nodeex.pNode)) //(!strcmp("pFeature",nodeex.szName))
+			{
+				szDomName = arv_dom_node_get_node_name(nodeex.pNode);
+				szFeature = arv_gc_feature_node_get_name((ArvGcFeatureNode *)nodeex.pNode);
+				szFeatureValue = arv_gc_feature_node_get_value_as_string((ArvGcFeatureNode *)nodeex.pNode, NULL);
+				if (szFeature && szFeatureValue && szFeatureValue[0])
+					ROS_INFO("FeatureName: %s%s, %s=%s", szIndent, szDomName, szFeature, szFeatureValue);
+			}
+			PrintDOMTree(pGenicam, nodeex, nIndent+4);
+			
+			// Go to the next sibling.
+			nodeex = GetGcNextSibling(pGenicam, nodeex);
+
+		} while (nodeex.pNode && nodeex.pNodeSibling);
+	}
+}
 int main(int argc, char** argv) 
 {
     char   *pszGuid = NULL;
@@ -731,6 +853,17 @@ int main(int argc, char** argv)
 
 			ROS_INFO ("    ---------------------------");
 			
+			
+//			ROS_INFO ("    ----------------------------------------------------------------------------------");
+//			NODEEX		 nodeex;
+//			ArvGc	*pGenicam=0;
+//			pGenicam = arv_device_get_genicam(global.pDevice);
+//
+//			nodeex.szName = "Root";
+//			nodeex.pNode = (ArvDomNode	*)arv_gc_get_node(pGenicam, nodeex.szName);
+//			nodeex.pNodeSibling = NULL;
+//			PrintDOMTree(pGenicam, nodeex, 0);
+//			ROS_INFO ("    ----------------------------------------------------------------------------------");
 			
 
 
